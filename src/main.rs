fe0304fd36fe556e978c29e673c439719ad4e2d5
Spencer Bartholomew <38776747+spencerbart@users.noreply.github.com>
2022-09-27 13:43:14 -0600

    logger and port number for railway

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use std::env;
+
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, middleware::Logger};
+use dotenv::dotenv;
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -16,13 +19,23 @@ async fn manual_hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+    env_logger::init();
+
+    let port_number: u16 = env::var("PORT")
+        .unwrap_or("8000".to_string())
+        .parse()
+        .unwrap();
     HttpServer::new(|| {
+
+        let logger = Logger::default();
         App::new()
+            .wrap(logger)
             .service(hello)
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", port_number))?
     .run()
     .await
 }
